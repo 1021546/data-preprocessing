92fb96474db14e2d62fd54c27aa8ff8849275b5a
1021546 <jerryhuang0312@gmail.com>
2018-01-31 14:50:53 +0800

    Delete write_filename.py

--- write_filename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-text_file = open("Output.txt", "w")
-for directory in os.listdir("."):
-	if os.path.isdir(directory):
-		for filename in os.listdir(directory):
-			if filename.endswith(".wav"):
-				text_file.write(filename+"\n")
-text_file.close()
